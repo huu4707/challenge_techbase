2e42a38283f0d18a0b321d8aeed2158dc2ec53d5
huu0909 <huu.nguyen@3forcom.com.vn>
2020-08-26 17:03:13 +0700

    test login

--- __tests__/user/user.e2e.ts
+++ __tests__/user/user.e2e.ts
@@ -1,16 +1,24 @@
 import 'jest-extended';
 import { SetupRequest } from '../../src/utils/testing';
+import { UserModel } from '../../src/repositories/models';
 const callAPI = new SetupRequest().request;
 import faker from 'faker';
 const randomEmail = faker.internet.email();
 
 beforeAll(async () => {
   // This hook excutes before run test cases
+  await UserModel.destroy({ where: {}, truncate: true });
+  const data = {
+    email: 'nguyenvana@gmail.com',
+    name: 'Nguyen Van A',
+    password: '123456Aa!'
+  };
+  await callAPI({ method: 'post', url: '/user/register', data });
 });
 
 jest.setTimeout(50000);
 
-describe('Register', () => {
+xdescribe('Register', () => {
   const expectedFailedResult = {
     errors: expect.toBeArray()
   };
@@ -33,7 +41,7 @@ describe('Register', () => {
     const data = {
       email: 'invalidEmail',
       name: 'test',
-      password: 'MyPass123!'
+      password: '123456Aa!'
     };
     const result = await callAPI({ method: 'post', url: '/user/register', data });
     expect(result.response).toMatchObject(expectedFailedResult);
@@ -43,7 +51,7 @@ describe('Register', () => {
   test('Missing name should be fail', async () => {
     const data = {
       email: randomEmail,
-      password: 'MyPass123!'
+      password: '123456Aa!'
     };
     const result = await callAPI({ method: 'post', url: '/user/register', data });
     expect(result.response).toMatchObject(expectedFailedResult);
@@ -70,87 +78,101 @@ describe('Register', () => {
     expect(result.response).toMatchObject(expectedFailedResult);
   });
 
-  // // Succesful case
-  test('Succesful case', async () => {
+  //Email already exists should be fail
+  test('Email already existsl must be fail', async () => {
+    const data = {
+      name: 'test',
+      email: 'nguyenvana@gmail.com',
+      password: '123456Aa!'
+    };
+    const result = await callAPI({ method: 'post', url: '/user/register', data });
+    expect(result.response).toMatchObject(expectedFailedResult);
+  });
+  // // Successful case
+  test('Successful case', async () => {
     const data = {
       email: randomEmail,
       name: 'MrA',
-      password: 'MyPass123!'
+      password: '123456Aa!'
     };
     const result = await callAPI({ method: 'post', url: '/user/register', data });
     expect(result.response).toMatchObject(expectedSuccessResult);
   });
 });
 
-// describe('Login', () => {
-//   const expectedFailedResult = {
-//     errors: expect.toBeArray()
-//   };
-//   const expectedSuccessResult = {
-//     data: expect.toBeObject(),
-//     meta: expect.toBeNil() || expect.toBeObject()
-//   };
-//   // Missing Email should be fail
-//   test('Missing email must be fail', async () => {
-//     const data = {
-//       email: 'nguyentienhuu47@gmail.com',
-//       password: '123456Aa!'
-//     };
-//     const result = await callAPI({ method: 'post', url: '/user/login', data });
-//     console.log('result', result);
-//     expect(result.response).toMatchObject(expectedFailedResult);
-//   });
+describe('Login', () => {
+  const expectedFailedResult = {
+    errors: expect.toBeArray()
+  };
+  const expectedSuccessResult = {
+    data: expect.toBeObject(),
+    meta: expect.toBeNil() || expect.toBeObject()
+  };
+  // Missing Email should be fail
+  test('Missing email must be fail', async () => {
+    const data = {
+      password: '123456Aa!'
+    };
+    const result = await callAPI({ method: 'post', url: '/user/login', data });
+    expect(result.response).toMatchObject(expectedFailedResult);
+  });
 
-//   // //Invalid Email
-//   // test('Email invalid format should be fail', async () => {
-//   //   const data = {
-//   //     email: 'invalidEmail',
-//   //     name: 'test',
-//   //     password: 'MyPass123!'
-//   //   };
-//   //   const result = await callAPI({ method: 'post', url: '/user/register', data });
-//   //   expect(result.response).toMatchObject(expectedFailedResult);
-//   // });
+  // //Invalid Email
+  test('Email invalid format should be fail', async () => {
+    const data = {
+      email: 'invalidEmail',
+      password: '123456Aa!'
+    };
+    const result = await callAPI({ method: 'post', url: '/user/login', data });
+    expect(result.response).toMatchObject(expectedFailedResult);
+  });
 
-//   // // //Missing name
-//   // test('Missing name should be fail', async () => {
-//   //   const data = {
-//   //     email: randomEmail,
-//   //     password: 'MyPass123!'
-//   //   };
-//   //   const result = await callAPI({ method: 'post', url: '/user/register', data });
-//   //   expect(result.response).toMatchObject(expectedFailedResult);
-//   // });
+  // // // Missing password should be fail
+  test('Missing password should be fail', async () => {
+    const data = {
+      email: 'somgthing@gmail.com'
+    };
+    const result = await callAPI({ method: 'post', url: '/user/login', data });
+    expect(result.response).toMatchObject(expectedFailedResult);
+  });
 
-//   // // // Missing password should be fail
-//   // test('Missing password should be fail', async () => {
-//   //   const data = {
-//   //     email: 'somgthing@gmail.com',
-//   //     name: 'MrA'
-//   //   };
-//   //   const result = await callAPI({ method: 'post', url: '/user/register', data });
-//   //   expect(result.response).toMatchObject(expectedFailedResult);
-//   // });
+  // // // Password invalid format
+  test('Missing password should be fail', async () => {
+    const data = {
+      email: 'somgthing@gmail.com',
+      password: '123'
+    };
+    const result = await callAPI({ method: 'post', url: '/user/login', data });
+    expect(result.response).toMatchObject(expectedFailedResult);
+  });
 
-//   // // // Password invalid format
-//   // test('Missing password should be fail', async () => {
-//   //   const data = {
-//   //     email: 'somgthing@gmail.com',
-//   //     name: 'MrA',
-//   //     password: '123'
-//   //   };
-//   //   const result = await callAPI({ method: 'post', url: '/user/register', data });
-//   //   expect(result.response).toMatchObject(expectedFailedResult);
-//   // });
+  //Email not found
+  test('Email not found', async () => {
+    const data = {
+      email: 'somgthing@gmail.com',
+      password: '123456Aa!'
+    };
+    const result = await callAPI({ method: 'post', url: '/user/login', data });
+    expect(result.response).toMatchObject(expectedFailedResult);
+  });
 
-//   // // // Succesful case
-//   // test('Succesful case', async () => {
-//   //   const data = {
-//   //     email: randomEmail,
-//   //     name: 'MrA',
-//   //     password: 'MyPass123!'
-//   //   };
-//   //   const result = await callAPI({ method: 'post', url: '/user/register', data });
-//   //   expect(result.response).toMatchObject(expectedSuccessResult);
-//   // });
-// });
+  //Password wrong
+  test('Password wrong', async () => {
+    const data = {
+      email: 'nguyenvana@gmail.com',
+      password: '123456Aa!a'
+    };
+    const result = await callAPI({ method: 'post', url: '/user/login', data });
+    expect(result.response).toMatchObject(expectedFailedResult);
+  });
+
+  // // Successful case
+  test('Successful case', async () => {
+    const data = {
+      email: 'nguyenvana@gmail.com',
+      password: '123456Aa!'
+    };
+    const result = await callAPI({ method: 'post', url: '/user/login', data });
+    expect(result.response).toMatchObject(expectedSuccessResult);
+  });
+});
